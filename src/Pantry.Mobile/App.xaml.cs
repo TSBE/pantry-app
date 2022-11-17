0191ff45c19c59643552050fcc934e08ddc80bd7
Px7-941 <tedbits@mailbox.org>
2022-11-17 15:20:28 +0100

    feat(#3): add app center for crash reporting

--- src/Pantry.Mobile/App.xaml.cs
+++ src/Pantry.Mobile/App.xaml.cs
@@ -1,12 +1,21 @@
-﻿namespace Pantry;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Pantry;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        AppCenter.Start(
+               "ios=10c794f1-e97e-424a-bb13-3938fce0a316;" +
+               "android=491d7d80-a368-4675-80a2-f87eea2d7cec;",
+               typeof(Analytics), typeof(Crashes));
+
+        MainPage = new AppShell();
+    }
 }
 

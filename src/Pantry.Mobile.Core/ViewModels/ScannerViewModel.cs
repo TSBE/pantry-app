ebb7a958c53cdeb038bb08c9d158077498f5e9cb
TedBits <tedbits@mailbox.org>
2023-10-26 00:04:56 +0200

    refactor(scanner): move code behind to viewmodel

---
+++ src/Pantry.Mobile.Core/ViewModels/ScannerViewModel.cs
@@ -0,0 +1,78 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Pantry.Mobile.Core.Infrastructure.Abstractions;
+using Pantry.Mobile.Core.Infrastructure.Helpers;
+using ZXing.Net.Maui;
+
+namespace Pantry.Mobile.Core.ViewModels;
+
+[QueryProperty(nameof(BackTargetPage), nameof(BackTargetPage))]
+public partial class ScannerViewModel : BaseViewModel
+{
+    private readonly INavigationService _navigation;
+
+    public ScannerViewModel(INavigationService navigation)
+    {
+        _navigation = navigation;
+    }
+    [ObservableProperty]
+    public string backTargetPage = string.Empty;
+    
+    [ObservableProperty]
+    public bool isTorchOn = false;
+    
+    [ObservableProperty]
+    public bool isDetecting = true;
+    
+    [ObservableProperty]
+    public BarcodeReaderOptions barcodeReaderOptions = new BarcodeReaderOptions
+    {
+        Formats = BarcodeFormat.Ean13, //| BarcodeFormat.QrCode,
+        AutoRotate = true,
+        Multiple = false,
+    };
+    
+    [RelayCommand]
+    public async Task BarcodesDetected(BarcodeDetectionEventArgs e)
+    {
+        try
+        {
+            IsDetecting = false;
+            var barcode = e.Results.FirstOrDefault();
+
+            if (lastBarcode.Equals(barcode?.Value))
+            {
+                return;
+            }
+
+            if (!GtinChecker.IsValidEAN13(barcode?.Value ?? string.Empty) && barcode?.Format == BarcodeFormat.Ean13)
+            {
+                return;
+            }
+
+            if (!Guid.TryParse(barcode?.Value, out _) && barcode?.Format == BarcodeFormat.QrCode)
+            {
+                return;
+            }
+
+            lastBarcode = barcode?.Value ?? string.Empty;
+
+            if (string.IsNullOrEmpty(BackTargetPage))
+            {
+                await MainThread.InvokeOnMainThreadAsync(()=>
+                    _navigation.GoToAsync($"..?Barcode={barcode?.Value}"));
+            }
+            else
+            {
+                await MainThread.InvokeOnMainThreadAsync(() =>
+                    _navigation.GoToAsync($"../{BackTargetPage}?Barcode={barcode?.Value}"));
+            }
+        }
+        finally
+        {
+            IsDetecting = true;
+        }
+    }
+    
+    private string lastBarcode = string.Empty;
+}
--- src/Pantry.Mobile/Views/ScannerPage.xaml.cs
+++ src/Pantry.Mobile/Views/ScannerPage.xaml.cs
@@ -1,87 +1,12 @@
-﻿using System.ComponentModel;
-using Pantry.Mobile.Core.Infrastructure.Abstractions;
-using Pantry.Mobile.Core.Infrastructure.Helpers;
-using ZXing.Net.Maui;
+﻿using Pantry.Mobile.Core.ViewModels;
 
 namespace Pantry.Mobile.Views;
 
-[QueryProperty(nameof(BackTargetPage), nameof(BackTargetPage))]
-public partial class ScannerPage : ContentPage, INotifyPropertyChanged
+public partial class ScannerPage
 {
-    private readonly INavigationService _navigationService;
-
-    public ScannerPage(INavigationService navigationService)
+    public ScannerPage(ScannerViewModel vm)
     {
         InitializeComponent();
-        BindingContext = this;
-
-        _navigationService = navigationService;
-
-        cameraBarcodeReaderView.Options = new BarcodeReaderOptions
-        {
-            Formats = BarcodeFormat.Ean13 | BarcodeFormat.QrCode,
-            AutoRotate = true,
-            Multiple = false
-        };
-    }
-
-    public string BackTargetPage { get; set; } = string.Empty;
-
-    private bool isDetecting = true;
-    public bool IsDetecting
-    {
-        get
-        {
-            return isDetecting;
-        }
-        set
-        {
-            isDetecting = value;
-            OnPropertyChanged();
-        }
-    }
-
-    private string lastBarcode = string.Empty;
-
-    protected void BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
-    {
-        try
-        {
-            IsDetecting = false;
-            var barcode = e.Results.FirstOrDefault();
-
-            if (lastBarcode.Equals(barcode?.Value))
-            {
-                return;
-            }
-
-            if (!GtinChecker.IsValidEAN13(barcode?.Value ?? string.Empty) && barcode?.Format == BarcodeFormat.Ean13)
-            {
-                return;
-            }
-
-            if (!Guid.TryParse(barcode?.Value, out _) && barcode?.Format == BarcodeFormat.QrCode)
-            {
-                return;
-            }
-
-            MainThread.BeginInvokeOnMainThread(async () =>
-            {
-                lastBarcode = barcode?.Value ?? string.Empty;
-
-                if (string.IsNullOrEmpty(BackTargetPage))
-                {
-                    await _navigationService.GoToAsync($"..?Barcode={barcode?.Value}");
-                }
-                else
-                {
-                    await _navigationService.GoToAsync($"../{BackTargetPage}?Barcode={barcode?.Value}");
-                }
-            });
-        }
-        finally
-        {
-            IsDetecting = true;
-        }
+        BindingContext = vm;
     }
 }

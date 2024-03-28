53d4091b027c8b217df3981d8e57097ee18eb6e5
TedBits <tedbits@mailbox.org>
2024-03-28 23:11:47 +0100

    refactor(#136): service provider registration

--- src/Pantry.Mobile/MauiProgram.cs
+++ src/Pantry.Mobile/MauiProgram.cs
@@ -2,13 +2,6 @@
 using CommunityToolkit.Maui;
 using CommunityToolkit.Maui.Core;
 using Microsoft.Extensions.Logging;
-using Pantry.Mobile.Core.Infrastructure;
-using Pantry.Mobile.Core.Infrastructure.Abstractions;
-using Pantry.Mobile.Core.Infrastructure.Auth0;
-using Pantry.Mobile.Core.Infrastructure.Services.PantryService;
-using Pantry.Mobile.Core.ViewModels;
-using Pantry.Mobile.Interactors;
-using Pantry.Mobile.Views;
 using ZXing.Net.Maui.Controls;
 
 namespace Pantry.Mobile;
@@ -34,70 +27,17 @@ public static class MauiProgram
             })
             .ConfigureMauiHandlers(handlers =>
             {
-                // Add the handlers
                 handlers.AddBarcodeScannerHandler();
-            });
-
+            })
 #if DEBUG
-        builder.Logging.AddDebug();
+            .Logging.AddDebug()
 #endif
-
-        // Register essentials
-        builder.Services.AddSingleton(VersionTracking.Default);
-        builder.Services.AddSingleton(Preferences.Default);
-        builder.Services.AddSingleton(SecureStorage.Default);
-        builder.Services.AddSingleton(Connectivity.Current);
-
-        // Register services
-        builder.Services.AddSingleton<INavigationService, ShellNavigationWrapper>();
-        builder.Services.AddSingleton<IDialogService, ToolkitDialogService>();
-        builder.Services.AddSingleton<ISettingsService, SettingsService>();
-
-        // Register pages
-        builder.Services.AddSingleton<OnboardingPage>();
-        builder.Services.AddSingleton<LoginPage>();
-        builder.Services.AddSingleton<MainPage>();
-        builder.Services.AddSingleton<SettingsPage>();
-        builder.Services.AddSingleton<CreateAccountPage>();
-        builder.Services.AddSingleton<HouseholdPage>();
-        builder.Services.AddSingleton<StorageLocationPage>();
-        builder.Services.AddTransient<ScannerPage>();
-        builder.Services.AddTransient<AddStorageLocationPage>();
-        builder.Services.AddTransient<AddArticlePage>();
-        builder.Services.AddTransient<ArticleDetailPage>();
-        builder.Services.AddTransient<ManageInvitationsPage>();
-
-        // Register view models
-        builder.Services.AddSingleton<OnboardingViewModel>();
-        builder.Services.AddSingleton<LoginViewModel>();
-        builder.Services.AddSingleton<MainViewModel>();
-        builder.Services.AddSingleton<SettingsViewModel>();
-        builder.Services.AddSingleton<CreateAccountViewModel>();
-        builder.Services.AddSingleton<HouseholdViewModel>();
-        builder.Services.AddSingleton<StorageLocationViewModel>();
-        builder.Services.AddTransient<ScannerViewModel>();
-        builder.Services.AddTransient<AddStorageLocationViewModel>();
-        builder.Services.AddTransient<AddArticleViewModel>();
-        builder.Services.AddTransient<ArticleDetailViewModel>();
-        builder.Services.AddTransient<ManageInvitationsViewModel>();
-
-        //Register helpers
-        builder.Services.AddSingleton<IKeyboardHelper, KeyboardHelper>();
-        builder.Services.AddSingleton<IAuth0Client>(new Auth0Client(new()
-        {
-            Audience = AppConstants.AUTH0_AUDIENCE,
-            Domain = AppConstants.AUTH0_DOMAIN,
-            ClientId = AppConstants.AUTH0_CLIENT_ID,
-            Scope = AppConstants.AUTH0_SCOPES,
-            RedirectUri = AppConstants.AUTH0_CALLBACK_URL,
-            Browser = new Pantry.Mobile.WebAuthenticator.WebBrowserAuthenticator()
-        }));
-#if DEBUG
-        builder.Services.AddSingleton<IPantryClientApiService>(new DummyPantryClientApiService());
-#else
-        builder.Services.AddRefreshTokenDelegatingHandler();
-        builder.Services.AddRefitClient<IPantryClientApiService>(AppConstants.PANTRY_BASEURL);
-#endif
+            .Services.RegisterEssentials()
+            .RegisterServices()
+            .RegisterPages()
+            .RegisterViewModels()
+            .RegisterHelpers();
+        
         return builder.Build();
     }
 }
---
+++ src/Pantry.Mobile/ServiceExtensions.cs
@@ -0,0 +1,79 @@
+﻿using Pantry.Mobile.Core.Infrastructure;
+using Pantry.Mobile.Core.Infrastructure.Abstractions;
+using Pantry.Mobile.Core.Infrastructure.Auth0;
+using Pantry.Mobile.Core.Infrastructure.Services.PantryService;
+using Pantry.Mobile.Core.ViewModels;
+using Pantry.Mobile.Interactors;
+using Pantry.Mobile.Views;
+
+namespace Pantry.Mobile;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection RegisterEssentials(this IServiceCollection service)
+    {
+        return service.AddSingleton(VersionTracking.Default)
+            .AddSingleton(Preferences.Default)
+            .AddSingleton(SecureStorage.Default)
+            .AddSingleton(Connectivity.Current);
+    }
+        
+    public static IServiceCollection RegisterServices(this IServiceCollection service)
+    {
+        return service.AddSingleton<INavigationService, ShellNavigationWrapper>()
+#if DEBUG
+            .AddSingleton<IPantryClientApiService>(new DummyPantryClientApiService())
+#else
+            .AddRefitClient<IPantryClientApiService>(AppConstants.PANTRY_BASEURL)
+#endif
+            .AddSingleton<IDialogService, ToolkitDialogService>()
+            .AddSingleton<ISettingsService, SettingsService>();
+    }
+        
+    public static IServiceCollection RegisterPages(this IServiceCollection service)
+    {
+        return service.AddSingleton<OnboardingPage>()
+            .AddSingleton<LoginPage>()
+            .AddSingleton<MainPage>()
+            .AddSingleton<SettingsPage>()
+            .AddSingleton<CreateAccountPage>()
+            .AddSingleton<HouseholdPage>()
+            .AddSingleton<StorageLocationPage>()
+            .AddTransient<ScannerPage>()
+            .AddTransient<AddStorageLocationPage>()
+            .AddTransient<AddArticlePage>()
+            .AddTransient<ArticleDetailPage>()
+            .AddTransient<ManageInvitationsPage>();
+    }
+        
+    public static IServiceCollection RegisterViewModels(this IServiceCollection service)
+    {
+        return service.AddSingleton<OnboardingViewModel>()
+            .AddSingleton<LoginViewModel>()
+            .AddSingleton<MainViewModel>()
+            .AddSingleton<SettingsViewModel>()
+            .AddSingleton<CreateAccountViewModel>()
+            .AddSingleton<HouseholdViewModel>()
+            .AddSingleton<StorageLocationViewModel>()
+            .AddTransient<ScannerViewModel>()
+            .AddTransient<AddStorageLocationViewModel>()
+            .AddTransient<AddArticleViewModel>()
+            .AddTransient<ArticleDetailViewModel>()
+            .AddTransient<ManageInvitationsViewModel>();
+    }
+        
+    public static IServiceCollection RegisterHelpers(this IServiceCollection service)
+    {
+        return service.AddSingleton<IKeyboardHelper, KeyboardHelper>()
+            .AddRefreshTokenDelegatingHandler()
+            .AddSingleton<IAuth0Client>(new Auth0Client(new()
+            {
+                Audience = AppConstants.AUTH0_AUDIENCE,
+                Domain = AppConstants.AUTH0_DOMAIN,
+                ClientId = AppConstants.AUTH0_CLIENT_ID,
+                Scope = AppConstants.AUTH0_SCOPES,
+                RedirectUri = AppConstants.AUTH0_CALLBACK_URL,
+                Browser = new Pantry.Mobile.WebAuthenticator.WebBrowserAuthenticator()
+            }));
+    }
+}

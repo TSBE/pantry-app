2f18a12e0cf378491ad77ed47c9dc56f98ab840a
Px7-941 <tedbits@mailbox.org>
2026-05-30 23:35:21 +0200

    feat: mock mode (#381)

--- src/Pantry.Mobile/Platforms/Android/MainActivity.cs
+++ src/Pantry.Mobile/Platforms/Android/MainActivity.cs
@@ -1,9 +1,25 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 
 namespace Pantry.Mobile;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity
-{
+[IntentFilter([Platform.Intent.ActionAppAction],
+    Categories = [global::Android.Content.Intent.CategoryDefault])]
+public class MainActivity : MauiAppCompatActivity {
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+
+        Platform.OnResume(this);
+    }
+
+    protected override void OnNewIntent(Intent? intent)
+    {
+        base.OnNewIntent(intent);
+
+        Platform.OnNewIntent(intent);
+    }
 }
